1ed610854817cf76e1e8c6c3258355625d8afb1b
Teslim Adeyanju <info@adeyanjuteslim.co.uk>
2025-02-22 21:03:41 +0000

    Updated .gitignore

--- airflow/dags/hello_world.py
+++ airflow/dags/hello_world.py
@@ -1,21 +1,23 @@
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2025, 2, 1),
+    'start_date': datetime(2024, 1, 1),  # Past date for immediate availability
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=1),
 }
 
-dag = DAG('hello_world', default_args=default_args, schedule_interval=timedelta(days=1))
+with DAG('hello_world',
+         default_args=default_args,
+         schedule_interval=timedelta(days=1),
+         catchup=False) as dag:
 
-t1 = BashOperator(
-    task_id='say_hello',
-    bash_command='echo "Hello World from Airflow!"',
-    dag=dag,
-)
+    t1 = BashOperator(
+        task_id='say_hello',
+        bash_command='echo "Hello World from Airflow!"'
+    )
